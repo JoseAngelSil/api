4f596963dea3c79b1da6d38f8eaf3c92779affc7
Jose Angel Silva <joseangel413silva@gmail.com>
2023-05-09 17:45:07 -0600

    ADD: CAP 04 Request Get
    SE usa el la anotacion @GetMapping para usar el metodo get en la calse MedicoController.java
    Se implementa la paginacion con la clase Page y Pageable, ver los metodos que tiene y como ordenar, anotar la cantidad de elementos por pagina y ordenarlos.
    Ademas se configuro el application.properties para que se visualizara los comandos de SQL que usa al ejecutar un metodo de la api, por lo que se muestra y se da el formato mas entendible para el proyecto.
    Se crea la clase DatosListadoMedico.java para que se almacene los datos a consultar con lo que utiliza la regla de negocio, por lo que es por mediante un contructor y que este admita n cantidad de medicos.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,17 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -18,5 +21,16 @@ public class MedicoController {
     @PostMapping
     public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
         medicoRepository.save(new Medico( datosRegistroMedico));
+    }
+
+    /**
+     * Metodo Listado Medico
+     * @return lista de medicos por medio de la tabla medicos por medio de una lista
+     * El parametro @GetMapping es el metodo para que envie lista es como consulta o un Read
+     * de modelo CRUD.
+     */
+    @GetMapping
+    public Page<DatosListadoMedico> ListadoMedico(@PageableDefault(size = 2) Pageable paginacion){
+        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 }

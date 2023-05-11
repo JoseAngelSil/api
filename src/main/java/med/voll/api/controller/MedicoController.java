c42bc83bdf4cfefd5f9fe248126de90d8481251e
Jose Angel Silva <joseangel413silva@gmail.com>
2023-05-11 15:18:11 -0600

    Add: Cap 01 Spring Boot 3. mejores practicas y seguridad
    Se usa la clase  ResponseEntyti de Spring para personalizar los retornos
    de los metodos de MedicoControler.java
    Se agrega en el paquete de medico DatosRespuestaMedico.java para que
    responda con un 200 o 201 de acuerdo que no afecte a la clase de la
    persistencia de JPA con Hibernate.
    
    Se personaliza los codigos HTTP mas apropiados para cada operacion.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,13 +2,17 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.direccion.DatosDireccion;
 import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,8 +21,20 @@ public class MedicoController {
     @Autowired
     private MedicoRepository medicoRepository;
     @PostMapping
-    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
-        medicoRepository.save(new Medico( datosRegistroMedico));
+    public ResponseEntity<DatosRespuestaMedico> registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico,
+                                          UriComponentsBuilder uriComponentsBuilder){
+        Medico medico =medicoRepository.save(new Medico( datosRegistroMedico));
+        // retorna un 201 created
+        //URL donde encontrar al medico GET http://localhost:8080/medicos/xxx
+        DatosRespuestaMedico resp = new DatosRespuestaMedico(medico.getId(),medico.getNombre(),medico.getEmail(),medico.getTelefono(),medico.getEspecialidad().toString(),
+                new DatosDireccion(medico.getDireccion().getCalle(),
+                        medico.getDireccion().getDistrito(),
+                        medico.getDireccion().getCiudad(),
+                        medico.getDireccion().getNumero(),
+                        medico.getDireccion().getComplemento()));
+        //URI url = "http://localhost:8080/medicos/"+medico.getId();
+        URI url = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(url).body(resp);
     }
 
     /**
@@ -28,24 +44,48 @@ public class MedicoController {
      * de modelo CRUD.
      */
     @GetMapping
-    public Page<DatosListadoMedico> ListadoMedico(@PageableDefault(size = 2) Pageable paginacion){
+    public ResponseEntity<Page<DatosListadoMedico>> ListadoMedico(@PageableDefault(size = 2) Pageable paginacion){
         //return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
-        return medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new);
+        return ResponseEntity.ok(medicoRepository.findByActivoTrue(paginacion).map(DatosListadoMedico::new));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DatosRespuestaMedico> buscarMedico(@PathVariable Long id){
+        Medico  medico = medicoRepository.getReferenceById(id);
+        // delete DB
+        //medicoRepository.delete(medico);
+        return ResponseEntity.ok(new DatosRespuestaMedico(
+                medico.getId(),medico.getNombre(),medico.getEmail(),medico.getTelefono(),medico.getEspecialidad().toString(),
+                new DatosDireccion(medico.getDireccion().getCalle(),
+                        medico.getDireccion().getDistrito(),
+                        medico.getDireccion().getCiudad(),
+                        medico.getDireccion().getNumero(),
+                        medico.getDireccion().getComplemento()))); // respondiendo con un codifo automatico como el 204
+    }
+
     @PutMapping
     @Transactional
-    public void modificarMedico(@RequestBody @Valid DatosActualizarMedicos datosActualizarMedico){
+    public ResponseEntity modificarMedico(@RequestBody @Valid DatosActualizarMedicos datosActualizarMedico){
         Medico  medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
         medico.actualizarDatos(datosActualizarMedico);
+        return ResponseEntity.ok(new DatosRespuestaMedico(
+                medico.getId(),medico.getNombre(),medico.getEmail(),medico.getTelefono(),medico.getEspecialidad().toString(),
+                new DatosDireccion(medico.getDireccion().getCalle(),
+                        medico.getDireccion().getDistrito(),
+                        medico.getDireccion().getCiudad(),
+                        medico.getDireccion().getNumero(),
+                        medico.getDireccion().getComplemento())
+        )); // Retorna un 200 pero con el objeto de la persona registrada
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deleteMedico(@PathVariable Long id){
+    public ResponseEntity deleteMedico(@PathVariable Long id){
         Medico  medico = medicoRepository.getReferenceById(id);
         // delete logico
         medico.desactivarMedico();
         // delete DB
         //medicoRepository.delete(medico);
+        return ResponseEntity.noContent().build(); // respondiendo con un codifo automatico como el 204
     }
 }

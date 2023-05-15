45056e6a04b10920cbfa9cfac89d66afb01e0869
Jose Silva <joseangel413silva@gmail.com>
2023-05-14 21:57:36 -0600

    Add: Cap 05 Control de acceso
    Se brinda control de acceso por medio de filtros, por lo que el
    SecurityFilter.java esta siendo heredado de OncePerRequestFilter de
    Spring.
    por medio de Auth0, los tokens son manejados para el control de la API

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,8 +1,11 @@
 package med.voll.api.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import med.voll.api.domain.usuarios.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,9 +16,14 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
+
+
     @Value("${api.security.secret}") // exportar el valor de la variable
     private String apiSecret;
+
     public String generarToken(Usuario usuario){
+
+
         String token;
         try {
             Algorithm algorithm = Algorithm.HMAC256(apiSecret);
@@ -30,6 +38,27 @@ public class TokenService {
             throw new RuntimeException();
         }
         return token;
+    }
+
+    public String getSubject(String token){
+        if (token == null) {
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret); // validando firma
+            verifier = JWT.require(algorithm)
+                    .withIssuer("voll med")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null) {
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
     }
 
     private Instant generarFechaExp(){
